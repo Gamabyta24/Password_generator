9701718b3fc46aaa67ee41cc73ec4e5a07a0e523
Denaid24 <Gamabyta@gmail.com>
2022-08-08 21:36:03 +0300

    add setup.cfg and new func

--- pass_gen/generator.py
+++ pass_gen/generator.py
@@ -5,53 +5,52 @@ from global_const import *
 
 def your_pass(password):
     print(f"Your password: {password}")
-
-
+def default_values(object_arg):
+    object_arg[DIGITS_OBJ] = False
+    object_arg[UPPERCASE_OBJ] = False
+    object_arg[LOWERCASE_OBJ] = False
+    object_arg[PUNCTUATION_OBJ] = False
+def all_values(object_arg):
+    object_arg[DIGITS_OBJ] = True
+    object_arg[UPPERCASE_OBJ] = True
+    object_arg[LOWERCASE_OBJ] = True
+    object_arg[PUNCTUATION_OBJ] = True
 def get_user_data():
-    objects_data = {
-        "lenght": 0,
-        "auto": 0,
-        "digits": 0,
-        "uppercase": 0,
-        "lowercase": 0,
-        "symbols": 0,
-    }
-    objects_data["lenght"] = prompt.integer("Enter pass lenght: ")
+    objects_data = {}
+    default_values(objects_data)
+    objects_data[LONG_OBJ] = prompt.integer("Enter pass lenght: ")
     if prompt.string("Gen auto? (y, n): ") == "y":
-        objects_data["auto"] = 1
-        objects_data["digits"] = 1
-        objects_data["uppercase"] = 1
-        objects_data["lowercase"] = 1
-        objects_data["symbols"] = 1
+        all_values(objects_data)
     else:
         if prompt.string("include digits? (y, n): ") == "y":
-            objects_data["digits"] = 1
+            objects_data[DIGITS_OBJ] = True
         if prompt.string("include uppercase (y, n): ") == "y":
-            objects_data["uppercase"] = 1
+            objects_data[UPPERCASE_OBJ] = True
         if prompt.string("include lowercase (y, n): ") == "y":
-            objects_data["lowercase"] = 1
+            objects_data[LOWERCASE_OBJ] = True
         if prompt.string("include symbols? (y, n): ") == "y":
-            objects_data["symbols"] = 1
+            objects_data[PUNCTUATION_OBJ] = True
     return objects_data
 
 
 def main():
+    """
+    Main function.
+    """
     user_data = get_user_data()
     chars = ""
-    if user_data["auto"] == 1:
-        chars += ALL_OPTION
-    if user_data["digits"] == 1:
+    if user_data[DIGITS_OBJ] == True:
         chars += DIGITS
-    if user_data["uppercase"] == 1:
+    if user_data[UPPERCASE_OBJ] == True:
         chars += UPPERCASE
-    if user_data["lowercase"] == 1:
+    if user_data[LOWERCASE_OBJ] == True:
         chars += LOWERCASE
-    if user_data["symbols"] == 1:
+    if user_data[PUNCTUATION_OBJ] == True:
         chars += PUNCTUATION
     print(user_data)
 
     password = ""
-    for i in range(user_data["lenght"]):
+    for i in range(user_data[LONG_OBJ]):
         password += choice(chars)
     your_pass(password)
 
--- pass_gen/global_const.py
+++ pass_gen/global_const.py
@@ -3,3 +3,9 @@ UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
 PUNCTUATION = "!#$%&*+-=?@^_"
 ALL_OPTION = DIGITS + UPPERCASE + LOWERCASE + PUNCTUATION
+
+DIGITS_OBJ = "digits"
+UPPERCASE_OBJ = "uppercase"
+LOWERCASE_OBJ = "lowercase"
+PUNCTUATION_OBJ = "symbols"
+LONG_OBJ = "long"
